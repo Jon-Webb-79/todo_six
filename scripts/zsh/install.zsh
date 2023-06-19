52bb92bc194f8e63654b5aeab0cd321d2d5ddc0c
Jon Webb <webbja123@gmail.com>
2023-06-19 08:18:07 -0600

    Updated installer paths

--- scripts/zsh/install.zsh
+++ scripts/zsh/install.zsh
@@ -17,11 +17,11 @@ else
 fi
 
 # Ensure permissions to PyQt files
-chmod +x /home/jonwebb/Desktop/todo_six/.venv/lib/python3.11/site-packages/PyQt6/QtSql.abi3.so
-chmod +x /home/jonwebb/Desktop/todo_six/.venv/lib/python3.11/site-packages/PyQt6/QtDBus.abi3.so
-chmod +x /home/jonwebb/Desktop/todo_six/.venv/lib/python3.11/site-packages/PyQt6/QtWidgets.abi3.so
-chmod +x /home/jonwebb/Desktop/todo_six/.venv/lib/python3.11/site-packages/PyQt6/QtGui.abi3.so
-chmod +x /home/jonwebb/Desktop/todo_six/.venv/lib/python3.11/site-packages/PyQt6/QtCore.abi3.so
+chmod +x .venv/lib/python3.11/site-packages/PyQt6/QtSql.abi3.so
+chmod +x .venv/lib/python3.11/site-packages/PyQt6/QtDBus.abi3.so
+chmod +x .venv/lib/python3.11/site-packages/PyQt6/QtWidgets.abi3.so
+chmod +x .venv/lib/python3.11/site-packages/PyQt6/QtGui.abi3.so
+chmod +x .venv/lib/python3.11/site-packages/PyQt6/QtCore.abi3.so
 
 # Transform to executable
 pyinstaller -F -w -i icon.ico --add-data "todo_six:todo_six" todo.py
